e3f11e1591728e0960f41ced922e563236a6950d
ht1625 <61890580+ht1625@users.noreply.github.com>
2022-04-08 01:14:54 +0300

    Update flower.kt

--- card_flower/app/src/main/java/com/example/card_flower/flower.kt
+++ card_flower/app/src/main/java/com/example/card_flower/flower.kt
@@ -1,13 +1,5 @@
 package com.example.card_flower
 
-class flower {
-    var title:String?=null
-    var text:String?=null
-    var img:Int?=null
-
-    constructor(title1:String,text1:String,img1:Int){
-        this.img=img1
-        this.text=text1
-        this.title=title1
-    }
+data class flowers (var title:String,var text:String, var img:Int){
+    
 }
